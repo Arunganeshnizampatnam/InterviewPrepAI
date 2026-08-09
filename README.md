35be4feae72530b37d1a1ff6fc1401b178cbb938
Arun Ganesh <arunganesh@gmail.com>
2026-08-09 13:58:07 +0530

    Updated README

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # InterviewPrepAI
-AI-powered Interview Preparation Platforms
+
+AI-Powered Interview Preparation Platform
+
+## Features
+
+- Aptitude Questions
+- Technical Questions
+- HR Interview Questions
+- Resume Analyzer
+
+## Technologies Used
+
+- Python
+- Flask
+- HTML
+- CSS
+
+## How to Run
+
+1. Install Flask
+
+   pip install flask
+
+2. Run the application
+
+   python app.py
+
+3. Open
+
+   http://127.0.0.1:5000
+
+## Author
+
+Arun Ganesh
--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template,request
 
 app = Flask(__name__)
 
@@ -6,5 +6,91 @@ app = Flask(__name__)
 def home():
     return render_template("index.html")
 
+@app.route("/aptitude")
+def aptitude():
+    return """
+    <h1>Aptitude Question</h1>
+    <p>What is 25 + 25?</p>
+
+    <button onclick="showAnswer()">Show Answer</button>
+
+    <p id="answer"></p>
+
+    <script>
+    function showAnswer() {
+        document.getElementById("answer").innerHTML = "Answer: 50";
+    }
+    </script>
+    """
+@app.route("/technical")
+def technical():
+    return """
+    <h1>Technical Question</h1>
+    <p>What is Python?</p>
+
+    <button onclick="showAnswer()">Show Answer</button>
+
+    <p id="answer"></p>
+
+    <script>
+    function showAnswer() {
+        document.getElementById("answer").innerHTML =
+        "Python is a high-level programming language.";
+    }
+    </script>
+    """
+@app.route("/hr")
+def hr():
+    return "<h1>HR Interview Questions</h1>"
+
+@app.route("/resume")
+def resume():
+    return """
+    <h1>Resume Analyzer</h1>
+
+    <form action="/upload" method="post"
+          enctype="multipart/form-data">
+
+        <input type="file" name="resume">
+
+        <br><br>
+
+        <input type="submit" value="Upload Resume">
+
+    </form>
+    """
+@app.route("/upload", methods=["POST"])
+def upload():
+    file = request.files["resume"]
+
+    file.save(file.filename)
+
+    score = 85
+
+    return f"""
+    <h1>Resume Analysis</h1>
+
+    <h2>Resume Uploaded Successfully!</h2>
+
+    <p><b>File:</b> {file.filename}</p>
+
+    <h3>Resume Score: {score}/100</h3>
+
+    <h3>Skills Detected</h3>
+    <ul>
+        <li>Python</li>
+        <li>Machine Learning</li>
+        <li>HTML</li>
+        <li>CSS</li>
+    </ul>
+
+    <h3>Suggestions</h3>
+    <ul>
+        <li>Add more projects</li>
+        <li>Include certifications</li>
+        <li>Improve technical skills section</li>
+    </ul>
+    """
 if __name__ == "__main__":
     app.run(debug=True)
+
